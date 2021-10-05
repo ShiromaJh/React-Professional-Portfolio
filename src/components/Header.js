15958364b5f8bbf8ce54b54d29114be62499b567
Justin Hoffman <justinhoffman193@gmail.com>
2021-10-05 16:07:58 -0500

    added finished landing page

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <div className="main-info">
+        <h1>Justin Hoffman</h1>
+        <Typed
+          className="typed-text"
+          strings={[
+            "Full Stack Developer",
+            "Music",
+            "Gaming",
+            "Computer Building",
+          ]}
+          typeSpeed={40}
+          backSpeed={75}
+          loop
+        />
+        <a href="#" className="btn-main">
+          Hire Me
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
