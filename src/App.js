fe66f5bef06a021eb93d725c487da0b77cd1277b
Justin Hoffman <justinhoffman193@gmail.com>
2021-10-05 19:52:46 -0500

    added contact function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from './components/About'
 import Projects from "./components/Projects";
+import Contact from "./components/contact";
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
       <Header />
       <About />
       <Projects />
+      <Contact />
     </>
   );
 }
